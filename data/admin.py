338d01e4afd8757c92e7c9be2ea28e950c743da5
maksi <uchebmaksimtok2006@yandex.ru>
2023-04-04 18:17:38 +0700

    Добавил в таблицы с админами колонку для номера телефона и общей информации

--- data/admin.py
+++ data/admin.py
@@ -8,3 +8,5 @@ class Admin(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=False)  # Пока будет не кэшированым а просто хранится здесь
+    number = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    info = sqlalchemy.Column(sqlalchemy.String, nullable=False)
