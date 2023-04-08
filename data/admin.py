e94a6c9a3fce972f86945cda3b5d5589825b6c04
maksi <uchebmaksimtok2006@yandex.ru>
2023-04-08 19:20:59 +0700

    Сделал html для главной страницы, регистрации, входа и базовый шаблон, но пока не подключил их. Также сделал шаблоны wtf форм для входа и решистрации, но сами страницы еще нет

--- data/admin.py
+++ data/admin.py
@@ -6,6 +6,7 @@ class Admin(SqlAlchemyBase):
     __tablename__ = 'admins'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=False)  # Пока будет не кэшированым а просто хранится здесь
     number = sqlalchemy.Column(sqlalchemy.String, nullable=False)
--- main.py
+++ main.py
@@ -1,12 +1,74 @@
-from flask import Flask
+from flask import Flask, render_template, make_response, request, session, redirect
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, StringField, EmailField, TextAreaField, TelField
+from wtforms.validators import DataRequired
 from data import db_session
+from data.admin import Admin
+from data.master import Master
+from data.process import Process
+from data.record import Record
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template
+from data import db_session
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class LoginForm(FlaskForm):
+    login = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Название организации', validators=[DataRequired()])
+    login = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    phone_number = TelField('Номер телофона организации', validators=[DataRequired()])
+    info = TextAreaField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query().get(user_id)
+
+
+def set_password(self, password):
+    self.hashed_password = generate_password_hash(password)
+
+
+def check_password(self, password):
+    return check_password_hash(self.hashed_password, password)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():  # Позже переделаю
+    pass
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():  # Позже переделаю
+    pass
 
 
 @app.route("/")
-def index():
+def index():  # Позже переделаю
     return "Hello, World!"
 
 
